6b45a06201dc7ccbbaa3c98b31acae3e8f49b6c7
Danilo Vega PC <danilo.vega@hotmail.es>
2023-04-08 18:17:24 -0500

    xamp

--- resources/views/livewire/show-posts.blade.php
+++ resources/views/livewire/show-posts.blade.php
@@ -26,82 +26,82 @@
             </div>
 
             @if (count($posts))
-                <table class="w-full min-w-full divide-y divide-gray-200">
-                    <thead class="bg-gray-50">
+            <table class="w-full min-w-full divide-y divide-gray-200">
+                <thead class="bg-gray-50">
+                    <tr>
+                        <th scope="col" class="cursor-pointer py-3 text-xs font-medium text-gray-500 uppercase tracking-wider border-b-2" wire:click="order('id')">
+                            ID
+                            @if ($sort == 'id')
+                                @if ($direction == 'asc')
+                                    <i class="fas fa-sort-alpha-up-alt mt-1"></i>
+                                @else
+                                    <i class="fas fa-sort-alpha-down-alt mt-1"></i>
+                                @endif
+                            @else
+                                <i class="fas fa-sort mt-1"></i>
+                            @endif
+                        </th>
+                        <th scope="col" class="cursor-pointer py-3 text-xs font-medium text-gray-500 uppercase tracking-wider border-b-2" wire:click="order('title')">
+                            Title
+                            @if ($sort == 'title')
+                                @if ($direction == 'asc')
+                                    <i class="fas fa-sort-alpha-up-alt mt-1"></i>
+                                @else
+                                    <i class="fas fa-sort-alpha-down-alt mt-1"></i>
+                                @endif
+                            @else
+                                <i class="fas fa-sort mt-1"></i>
+                            @endif                            
+                        </th>
+                        <th scope="col" class="cursor-pointer py-3 text-xs font-medium text-gray-500 uppercase tracking-wider border-b-2" wire:click="order('content')">
+                            Content
+                            @if ($sort == 'content')
+                                @if ($direction == 'asc')
+                                    <i class="fas fa-sort-alpha-up-alt mt-"></i>
+                                @else
+                                    <i class="fas fa-sort-alpha-down-alt mt-"></i>
+                                @endif
+                            @else
+                                <i class="fas fa-sort mt-1"></i>
+                            @endif
+                        </th> 
+                        <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider border-b-2">
+                            <span class="sr-only">Acciones</span>
+                        </th>
+                    </tr>
+                </thead>
+                <tbody class="bg-white divide-y divide-gray-200">
+                    @foreach ($posts as $item)
                         <tr>
-                            <th scope="col" class="cursor-pointer py-3 text-xs font-medium text-gray-500 uppercase tracking-wider border-b-2" wire:click="order('id')">
-                                ID
-                                @if ($sort == 'id')
-                                    @if ($direction == 'asc')
-                                        <i class="fas fa-sort-alpha-up-alt mt-1"></i>
-                                    @else
-                                        <i class="fas fa-sort-alpha-down-alt mt-1"></i>
-                                    @endif
-                                @else
-                                    <i class="fas fa-sort mt-1"></i>
-                                @endif
-                            </th>
-                            <th scope="col" class="cursor-pointer py-3 text-xs font-medium text-gray-500 uppercase tracking-wider border-b-2" wire:click="order('title')">
-                                Title
-                                @if ($sort == 'title')
-                                    @if ($direction == 'asc')
-                                        <i class="fas fa-sort-alpha-up-alt mt-1"></i>
-                                    @else
-                                        <i class="fas fa-sort-alpha-down-alt mt-1"></i>
-                                    @endif
-                                @else
-                                    <i class="fas fa-sort mt-1"></i>
-                                @endif                            
-                            </th>
-                            <th scope="col" class="cursor-pointer py-3 text-xs font-medium text-gray-500 uppercase tracking-wider border-b-2" wire:click="order('content')">
-                                Content
-                                @if ($sort == 'content')
-                                    @if ($direction == 'asc')
-                                        <i class="fas fa-sort-alpha-up-alt mt-"></i>
-                                    @else
-                                        <i class="fas fa-sort-alpha-down-alt mt-"></i>
-                                    @endif
-                                @else
-                                    <i class="fas fa-sort mt-1"></i>
-                                @endif
-                            </th> 
-                            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider border-b-2">
-                                <span class="sr-only">Acciones</span>
-                            </th>
-                        </tr>
-                    </thead>
-                    <tbody class="bg-white divide-y divide-gray-200">
-                        @foreach ($posts as $item)
-                            <tr>
-                                <td class="px-6 py-4 border-b-2">
-                                    <div class="text-sm font-medium text-gray-900">{{ $item->id }}</div>
-                                </td>
-                                <td class="px-6 py-4 border-b-2">
-                                    <div class="text-sm text-gray-500">{{ $item->title }}</div>
-                                </td>
-                                <td class="px-6 py-4 border-b-2">
-                                    <div class="text-sm text-gray-500">{!! $item->content !!}</div>
-                                </td>
-                                <td class="px-6 py-4 text-sm font-medium border-b-2 flex">
-                                    {{-- @livewire('edit-post', ['post' => $post], key($post->id)) --}}
-                                    <a class="btn btn-green" wire:click="edit({{ $item }})">
-                                        <i class="fas fa-edit"></i>
-                                    </a>
+                            <td class="px-6 py-4 border-b-2">
+                                <div class="text-sm font-medium text-gray-900">{{ $item->id }}</div>
+                            </td>
+                            <td class="px-6 py-4 border-b-2">
+                                <div class="text-sm text-gray-500">{{ $item->title }}</div>
+                            </td>
+                            <td class="px-6 py-4 border-b-2">
+                                <div class="text-sm text-gray-500">{!! $item->content !!}</div>
+                            </td>
+                            <td class="px-6 py-4 text-sm font-medium border-b-2 flex">
+                                {{-- @livewire('edit-post', ['post' => $post], key($post->id)) --}}
+                                <a class="btn btn-green" wire:click="edit({{ $item }})">
+                                    <i class="fas fa-edit"></i>
+                                </a>
 
-                                    <a class="btn btn-red ml-2" wire:click="$emit('deletePost', {{ $item->id }})">
-                                        <i class="fas fa-trash"></i>
-                                    </a>
-                                </td>
-                            </tr>   
-                        @endforeach                 
-                    </tbody>
-                </table>
+                                <a class="btn btn-red ml-2" wire:click="$emit('deletePost', {{ $item->id }})">
+                                    <i class="fas fa-trash"></i>
+                                </a>
+                            </td>
+                        </tr>   
+                    @endforeach                 
+                </tbody>
+            </table>
 
-                @if ($posts->hasPages())
-                <div class="px-6 py-3">
-                    {{ $posts->links() }}
-                </div>                
-                @endif
+            @if ($posts->hasPages())
+            <div class="px-6 py-3">
+                {{ $posts->links() }}
+            </div>                
+            @endif
 
             @else
                 <div class="px-6 py-4">No existen registros almacenados.</div>
